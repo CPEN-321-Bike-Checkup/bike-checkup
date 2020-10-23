543437d5df669886b389649ab626347f6a82149c
Connor Shannon <connormshannon@gmail.com>
2020-10-23 10:53:17 -0700

    Added Navigation Stack for screen controll

--- app/client/components/HomeScreen.js
+++ app/client/components/HomeScreen.js
@@ -17,20 +17,8 @@ export default class HomeScreen extends React.Component{
             <>
                 <View style={{flex: 1, flexDirection: 'column', alignItems: 'center', justifyContent: 'space-around', paddingVertical: 50}}>
                     <Button
-                        title="View My hometown"
-                        onPress={() => this.props.navigation.navigate('MapsScreen')} 
-                    />
-                    <Button
                         title="Get current time"
                         onPress={() => this.props.navigation.navigate('TimeScreen')} 
-                    />
-                    <Button
-                        title="Supprise Function"
-                        onPress={() => this.props.navigation.navigate('OtherFunction')} 
-                    />
-                    <Button
-                        title="Burndown Chart"
-                        onPress={() => this.props.navigation.navigate('BurndownChart')} 
                     />
                 </View>
             </>
