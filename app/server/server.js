982fc93e34b6ca6e3bb42e2043c20a04887f2ba7
Brennan <brennancathcart@gmail.com>
2020-11-13 14:50:30 -0800

    Hard-coded test data creation

--- app/server/server.js
+++ app/server/server.js
@@ -53,3 +53,75 @@ app.get('/stravaRedirect', function (req, res, next) {
   console.log('Strava Auth Hit');
   res.send('OK');
 });
+
+let createTestData = async function (userId) {
+  const userRepository = require('./repositories/UserRepository');
+
+  let record = {
+    description: 'Oil chain',
+    maintenance_date: new Date(),
+  };
+
+  let record2 = {
+    description: 'Replace chain',
+    maintenance_date: new Date(),
+  };
+
+  let component1 = {
+    label: 'KMC X11EL-1 Chain',
+    attatchment_date: new Date(),
+    removal_date: new Date(),
+    maintenance_records: [record, record2],
+    predicted_maintenance_date: new Date(),
+  };
+
+  let record3 = {
+    description: 'Bleed brakes',
+    maintenance_date: new Date(),
+  };
+
+  let component2 = {
+    label: 'Shimano 105 Hydraulic Disc, 160mm',
+    attatchment_date: new Date(),
+    removal_date: new Date(),
+    maintenance_records: [record3],
+    predicted_maintenance_date: new Date(),
+  };
+
+  // let bike = {
+  //   id: 25,
+  //   label: 'Norco Sasquatch',
+  //   components: [component],
+  // };
+
+  let bike1 = {
+    _id: 'b8294806',
+    owner: userId,
+    label: "Rachel's Hybrid Bike",
+    components: [component1],
+    __v: 0,
+  };
+  let bike2 = {
+    _id: 'b8294804',
+    owner: userId,
+    label: "Rachel's Road Bike",
+    components: [component2],
+    __v: 0,
+  };
+
+  let user = {
+    _id: userId,
+    name: 'Rachel Smith',
+    strava_token: '348672a87ebc0e4444b4a81961b32fdd75a761fc',
+    refresh_token: '2868f45d72e20be5fa737eb69ba1dc618dcbb495',
+    expires_in: 17932,
+    bikes: [bike1, bike2],
+    deviceTokens: [],
+    __v: 0,
+  };
+
+  userRepository.Create(user);
+};
+
+const userId = 123;
+// createTestData(userId);
