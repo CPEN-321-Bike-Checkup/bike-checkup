11520316af6c48891ef907b3786d51367dc2c51f
Brennan <brennancathcart@gmail.com>
2020-11-24 21:42:08 -0800

    Removing hardcoded values for my local testing

--- app/client/App.js
+++ app/client/App.js
@@ -17,7 +17,7 @@ import ActivitiesStack from './components/Stacks/ActivitiesStack';
 import Keys from './keys.json';
 
 // Dev debug flags
-const SKIP_AUTHENTICATION = true; // Set to false before committing!
+const SKIP_AUTHENTICATION = false; // Set to false before committing!
 
 const Tab = createMaterialBottomTabNavigator();
 
@@ -28,8 +28,8 @@ const CODE_LABEL_LENGTH = 5;
 const PARAM_SEPARATOR_LENGTH = 1;
 
 // Push notification configuration
-// const serverIp = '3.97.53.16';
-const serverIp = '192.168.1.83';  // Brennan's local IP
+const serverIp = '3.97.53.16';
+// const serverIp = '192.168.1.83';  // Brennan's local IP
 const senderID = 517168871348;
 
 export default class App extends Component {
