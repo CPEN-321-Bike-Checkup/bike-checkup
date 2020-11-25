bbf14f9cf1f304e0240a1acce5def3c2226c8e0c
Cindy Yuan <xinningyuan@hotmail.com>
2020-11-24 19:43:26 -0800

    added back prediction for component, searches activities by componentID in prediction algo

--- app/server/services/MaintenanceTaskService.js
+++ app/server/services/MaintenanceTaskService.js
@@ -161,14 +161,27 @@ class MaintenanceTaskService {
     });
   }
 
+  async MaintenancePredictForComponent(componentId) {
+    console.log('predicting for component...'); //DEBUG
+    let maintenanceList = await maintenanceTaskRepository.GetMaintenanceTasksForComponents(
+      componentId,
+    );
+
+    this.MaintenancePredict(maintenanceList);
+  }
+
   //TODO make the equivalent for component and use that function to do this for user
   async MaintenancePredictForUser(userId) {
-    //remove mock data later
-
+    console.log('predicting for user...'); //DEBUG
     let maintenanceList = await maintenanceTaskRepository.GetMaintenanceTasksForUser(
       userId,
     );
 
+    this.MaintenancePredict(maintenanceList);
+  }
+
+  //TODO make the equivalent for component and use that function to do this for user
+  async MaintenancePredict(maintenanceList) {
     maintenanceList = maintenanceList.filter(function (value, index, arr) {
       return arr[index].schedule_type == 'distance';
     });
@@ -211,19 +224,11 @@ class MaintenanceTaskService {
       return mean_y - slope * mean_x;
     }
 
-    //get JSON data from Strava call
-    //normalize units if necessary -> convert-units or just mile->km manual conversion
     //linear regression taken from: https://machinelearningmastery.com/implement-simple-linear-regression-scratch-python/
     var predict_dates = [];
     var predictionText = '';
     var maint_index;
     for (maint_index = 0; maint_index < maintenanceList.length; maint_index++) {
-      //no predictions to be made if schedule is not distance based
-      if (maintenanceList[maint_index].schedule_type != 'distance') {
-        predict_dates.push(null);
-        continue;
-      }
-
       //retrieve all activities since maintenance day for specified component
       var last_maint_date = maintenanceList[
         maint_index
@@ -314,5 +319,6 @@ const maintenanceTaskService = new MaintenanceTaskService(
   activityRepository,
   maintenanceTaskRepository,
   maintenanceRecordRepository,
+  componentActivityRepository,
 );
 module.exports = maintenanceTaskService;
